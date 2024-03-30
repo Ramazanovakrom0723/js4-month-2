01e4b3f98f07df70046e54490b16d386b297ef02
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-03-30 13:23:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,126 @@
+// 1-misol
+
+// let Random_num = parseInt(Math.random() * 100)
+// if (Random_num % 2 == 0) {
+//     console.log(`${Random_num} juft son`)
+// }else{
+//     console.log(`${Random_num} toq son`)
+// }
+
+
+
+// 2-misol
+
+// let prompt_num = prompt("...Enter your number")
+// if (prompt_num % 4 === 0) {
+//     if (prompt_num % 6 === 0) {
+//         console.log("4 va 6ga bolinadi")
+//     }else{
+//         console.log( "4ga bolinadi 6ga bolinmaydi" );
+//     }
+// }else
+//     if (prompt_num % 6 === 0) {
+//         console.log("6ga bolinadi 4ga bolinmaydi")
+    
+// }else{
+//     console.log("4 va 6ga ham bolinmaydi");
+// }
+
+
+
+
+
+// 3-misol
+
+// let prompt_num = prompt("...Enter your number")
+// if (prompt_num >= 18) {
+//     console.log("Hush kelibsiz");
+// }else{
+//     console.log("Uzur uka yoshiz yetmadi");
+// }
+
+
+
+// 4-misol
+
+// let prompt_num = prompt("...Enter your number")
+// let prompt_num2 = prompt("...Enter your number")
+// if (prompt_num > prompt_num2) {
+//     console.log(`${prompt_num} kotta`);
+// } else if (prompt_num < prompt_num2) {
+//     console.log(`${prompt_num2} kotta`)
+// }
+
+
+
+
+// 5-misol
+
+// let prompt_num = prompt("...Enter your number")
+// if (prompt_num > 1000) {
+//     console.log(`${prompt_num} som pulizga sizga 1000ga lux hona berolimz`);
+// } else if (1000 < prompt_num < 500 ) {
+//     console.log(`${prompt_num2} som pulizga sizga yarim lux hona bera olamz`)
+// } else if (500 < prompt_num < 200) {
+//     console.log(`${prompt_num2} som pulizga sizga oddiy hona bera olamz`)
+// } else if (prompt_num < 200) {
+//     console.log(`${prompt_num2} som pulizga xona bera olmaymiz`);
+// }
+
+
+
+
+
+// 6-misol
+
+// let prompt_num = prompt("...Enter your number")
+// if (prompt_num % 3 === 0) {
+//     if (prompt_num % 5 === 0) {
+//         console.log("fizz-bass")
+//     }else{
+//         console.log( "fizz" );
+//     }
+// }else
+//     if (prompt_num % 5 === 0) {
+//         console.log("bass")
+// }
+
+
+
+
+
+
+// 7-misol
+
+// let prompt_num = prompt("...Enter your prosent")
+// if (100 < prompt_num > 70) {
+//     console.log(`${prompt_num} grant `);
+// } else if (70 < prompt_num > 50 ) {
+//     console.log(`${prompt_num2} contract`)
+// } else if (prompt_num > 50) {
+//     console.log(`${prompt_num2} yiqildi`);
+// }
+
+
+
+
+
+// 8-misol
+
+// let prompt_num = prompt("...Enter your prosent")
+// let prompt_num2 = prompt("...Enter your prosent")
+// if (prompt_num > 9) {
+//     console.log("2 honali son");
+// }else{
+//     console.log("2 honali son emas");
+// }
+// if (prompt_num2 > 9) {
+//     console.log("2 honali son");
+// }else{
+//     console.log("2 honali son emas");
+// }
+// if (prompt_num > prompt_num2) {
+//     console.log(`${prompt_num} ${prompt_num2}`)
+// }else{
+//     console.log(`${prompt_num} ${prompt_num2}`)
+// }
